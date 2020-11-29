0aba1fc00a8e8d17fadf655f40a8e7d122d9a4c3
Nguyen Anh Tuan <anhtuan101294@gmail.com>
2020-11-29 21:36:24 +0700

    homework

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-// $(window).ready(function (e) {
-//    $('.course a').on('mouseenter', function(){
-//        toggle('overlay');
-//        console.log('mouse in')
-//    })
-// })
+async function init() {
+    let res = await fetch('https://www.cfdtraining.vn/api/danh-sach-khoa-hoc')
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+        })
+    let html = '';
+    for (let i in res) {
+        let thubnail = JSON.parse(res[i].thubnail);
+        html += `<div class="item col-4">
+        <div class="cover">
+            <img src="https://www.cfdtraining.vn/${thubnail.link}" alt="">
+        </div>
+        <div class="title">${res[i].title}</div>
+        <div class="content">${res[i].short_description}</div>
+    </div>`
+
+    }
+    console.log(html)
+    document.querySelector('.course-list').innerHTML = html;
+}
+init();
